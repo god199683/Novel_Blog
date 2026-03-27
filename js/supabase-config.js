1a92cb00d38454ee2a9e8ffcc45092808a725130
ciel <god199683@gmail.com>
2026-03-28 01:20:05 +0900

    7-2

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,9 +4,6 @@ const supabaseKey = sb_publishable_9N3fMtORoQRA7nsFCOLXGw__S9IHkbs;
 
 // CDN 버전 2 기준 초기화 방식
 const { createClient } = supabase;
-const _supabase = createClient(supabaseUrl, supabaseKey);
+window.supabaseClient = createClient(supabaseUrl, supabaseKey);
 
-// 다른 파일에서 사용할 수 있도록 전역 변수로 할당
-window.supabase = _supabase;
-
-console.log("Supabase 연결 설정 완료");
+console.log("Supabase 클라이언트 초기화 완료");
