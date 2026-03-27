af0557702c477e1c3218b1fa2f7eabff78be7149
god199683 <god199683@gmail.com>
2026-03-28 03:18:17 +0900

    Delete js directory

--- js/supabase-config.js
+++
@@ -1,6 +0,0 @@
-// js/supabase-config.js
-const supabaseUrl = 'https://smmirfcmpufnhsemduoc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtbWlyZmNtcHVmbmhzZW1kdW9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTU1MzEsImV4cCI6MjA5MDE5MTUzMX0.fYe_LLqL-FxYyxexY3inl1rU_au3v8ffeVGt-3iG5lM'; // <--- 이 부분이 엄청 길어도 정상입니다!
-
-const { createClient } = supabase;
-window.supabaseClient = createClient(supabaseUrl, supabaseKey);
