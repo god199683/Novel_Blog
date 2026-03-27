09947b85dce1d8283c5f95487a45161910e4b887
ciel <god199683@gmail.com>
2026-03-28 01:27:50 +0900

    7-5

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,6 @@
 // js/supabase-config.js
 const supabaseUrl = https://smmirfcmpufnhsemduoc.supabase.co;
-const supabaseKey = sb_publishable_9N3fMtORoQRA7nsFCOLXGw__S9IHkbs;
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtbWlyZmNtcHVmbmhzZW1kdW9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTU1MzEsImV4cCI6MjA5MDE5MTUzMX0.fYe_LLqL-FxYyxexY3inl1rU_au3v8ffeVGt-3iG5lM;
 
 // CDN 버전 2 기준 초기화 방식
 const { createClient } = supabase;
