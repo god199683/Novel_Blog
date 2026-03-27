afa36ceef64b419979c1fb23e7f39d8d3bdb5c8c
ciel <god199683@gmail.com>
2026-03-28 00:46:06 +0900

    3-1

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -5,7 +5,7 @@ const supabaseUrl = https://smmirfcmpufnhsemduoc.supabase.co
 const supabaseKey = sb_publishable_9N3fMtORoQRA7nsFCOLXGw__S9IHkbs
 
 // 2. Supabase 클라이언트 초기화
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = supabasejs.createClient(supabaseUrl, supabaseKey);
 
 // 연결 확인용 콘솔 로그 (나중에 지우셔도 됩니다)
 console.log("Supabase 연결 준비 완료!", supabase);
