2d7612d1456913f936759c4bec9c0024f79b7c2a
ciel <god199683@gmail.com>
2026-03-28 01:30:39 +0900

    7-5

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,9 +1,6 @@
 // js/supabase-config.js
 const supabaseUrl = 'https://smmirfcmpufnhsemduoc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtbWlyZmNtcHVmbmhzZW1kdW9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTU1MzEsImV4cCI6MjA5MDE5MTUzMX0.fYe_LLqL-FxYyxexY3inl1rU_au3v8ffeVGt-3iG5lM';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtbWlyZmNtcHVmbmhzZW1kdW9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTU1MzEsImV4cCI6MjA5MDE5MTUzMX0.fYe_LLqL-FxYyxexY3inl1rU_au3v8ffeVGt-3iG5lM'; // <--- 이 부분이 엄청 길어도 정상입니다!
 
-// CDN 버전 2 기준 초기화 방식
 const { createClient } = supabase;
 window.supabaseClient = createClient(supabaseUrl, supabaseKey);
-
-console.log("Supabase 클라이언트 초기화 완료");
